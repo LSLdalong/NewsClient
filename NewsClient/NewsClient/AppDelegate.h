691562ee28a79eecf21b6bbd7c09821d4e59e3a1
LSL <405585058@qq.com>
2015-11-25 15:17:16 +0800

    第一次

---
+++ NewsClient/NewsClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsClient
+//
+//  Created by 大龙 on 15/11/25.
+//  Copyright © 2015年 大龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsClient/NewsClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsClient
+//
+//  Created by 大龙 on 15/11/25.
+//  Copyright © 2015年 大龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
